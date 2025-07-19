cbc5248eb3da53f41ebbe6082609b804a3dae7df
masonville17 <anoniboni9876@gmail.com>
2025-07-19 09:59:49 +0000

    quick test. I saw these in logs last evening and they had been bothering me

--- helpers/fileconversion.ps1
+++ helpers/fileconversion.ps1
@@ -132,14 +132,14 @@ function Get-EmbeddedFilesFromHtml {
 
         try {
             [IO.File]::WriteAllBytes($filepath, [Convert]::FromBase64String($b64))
-            $results.ExternalFiles += $filename
+            $results.ExternalFiles += $(join-path $baseDir $filename)
             $results.Base64Images  += "data:image/$type;base64,..."
-            $results.Base64ImagesWritten += $filename
+            $results.Base64ImagesWritten += $(join-path $baseDir $filename)
 
             return "<img$($match.Groups[1].Value)src='$filename'"
         } catch {
             Write-Warning "Failed to decode embedded image: $($_.Exception.Message)"
-            return "<img$($match.Groups[1].Value)src=''"
+            return "<img$($match.Groups[1].Value)src='$filename'"
         }
     })
     $skipExts = @(

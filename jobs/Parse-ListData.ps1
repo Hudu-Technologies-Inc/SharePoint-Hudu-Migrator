7b723ad6b87182915536ce048470b7a3c1ed9d12
masonville17 <anoniboni9876@gmail.com>
2025-07-25 17:41:39 +0000

    still getting some 500 errors on asset layouts, I think if an asset layout of same name exists, we'll have to create a new one in case an existing layout already has assets by the same name

--- jobs/Parse-ListData.ps1
+++ jobs/Parse-ListData.ps1
@@ -4,7 +4,9 @@ if ($RunSummary.SetupInfo.SPListsAsLayouts) {
         $layoutName="$($list.SiteName)-$($list.ListName)"
         Set-PrintAndLog -message  "searching for or creating asset layout- $layoutName"
         $layoutIcon = $FontAwesomeIconMap[$(Select-ObjectFromList -allowNull $false -objects @($FontAwesomeIconMap.Keys) -message "Which Icon for layout $layoutName?")]
-        $layoutcolor = $HexColorMap[$(Select-ObjectFromList -allowNull $false -objects @($HexColorMap.Keys) -message "Choose a color for $layoutName with icon $layoutIcon")]
+        $colorOptions = $HexColorMap.Keys | Sort-Object
+        $selectedColorName = Select-ObjectFromList -allowNull $false -objects $colorOptions -message "Choose a color for $layoutName with icon $layoutIcon"
+        $layoutColor = $HexColorMap[$selectedColorName]
         $layoutBackgroundColor = Get-ComplimentingBackgroundColor -HexColor $layoutcolor
         $TempLayoutFields = @(@{label        = 'Imported from SharePoint'
                                 field_type   = 'Date'
@@ -16,12 +18,36 @@ if ($RunSummary.SetupInfo.SPListsAsLayouts) {
                                 show_in_list = 'false'
                                 position     = 502})        
     
-        $AssetLayout=$($(Get-HuduAssetLayouts -name "$layoutName") ?? $(New-HuduAssetLayout -name "$layoutName" -icon $layoutIcon -color $layoutBackgroundColor -icon_color $layoutcolor  -include_passwords $true -include_photos $true -include_comments $true -include_files $true -fields $TempLayoutFields )).assetlayout
+        $AssetLayout = Get-HuduAssetLayouts -name "$layoutName"
+
+        if (-not $AssetLayout) {
+            Set-PrintAndLog -message "Creating layout $layoutName with icon $layoutIcon, background $layoutBackgroundColor, icon color $layoutColor and tempfields $($TempLayoutFields | ConvertTo-Json)"
+
+            $AssetLayout = New-HuduAssetLayout -name "$layoutName" `
+                -icon $layoutIcon `
+                -color $layoutBackgroundColor `
+                -icon_color $layoutColor `
+                -include_passwords $true `
+                -include_photos $true `
+                -include_comments $true `
+                -include_files $true `
+                -fields $TempLayoutFields
+        }
+        $AssetLayout = $AssetLayout.assetlayout
+
+
         Set-PrintAndLog -message  "Layout Id $($AssetLayout.id) with $($list.Fields.Count) Fields and $($list.Values.Count) Values and $($list.LinkedFiles.Count) linked files..."
+
+
         $layoutFields = @()
         $PosIDX = 500
 
         foreach ($field in $list.Fields.Values) {
+            if (-not $field.HuduFieldType -or -not $field.Name) {
+                Set-PrintAndLog -message "Skipping invalid field with null type or name"
+                continue
+            }
+
             $newField = @{
                 field_type   = $field.HuduFieldType
                 label        = $field.Name
@@ -32,9 +58,9 @@ if ($RunSummary.SetupInfo.SPListsAsLayouts) {
             }
 
             if ($field.HuduFieldType -eq "dropdown" -or $field.HuduFieldType -eq "checkboxes") {
-                Set-PrintAndLog -message  "Found $($field.Choices.Count) choices in field '$($field.Name)'"
+                Set-PrintAndLog -message "Found $($field.Choices.Count) choices in '$($field.Name)'"
                 $newField += @{
-                    options          = ($field.Choices -join ", ")
+                    options          = $field.Choices
                     multiple_options = $field.MultipleChoice
                 }
             }
@@ -42,7 +68,11 @@ if ($RunSummary.SetupInfo.SPListsAsLayouts) {
             $layoutFields += $newField
             $PosIDX -= 1
         }
-        $AssetLayout = $(Set-HuduAssetLayout -id $AssetLayout.Id -name $AssetLayout.Name -fields $layoutFields).assetlayout 
+        $layoutFields | ConvertTo-Json -Depth 10 | Out-File "$(join-path $logsFolder -ChildPath "debug-fields-$layoutName.json")" 
+
+        $LayoutObject = Set-HuduAssetLayout -id $AssetLayout.Id -name $AssetLayout.Name -fields @($layoutFields)
+        $AssetLayout = $LayoutObject.assetlayout
+
         # $relationsToResolve=if ($list.LinkedFiles.Count -gt 0){
         #     $PosIDX=$PosIDX-1
         #     $layoutFields += @{
